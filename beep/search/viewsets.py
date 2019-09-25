eb914d4c48bb66dca2f23c37b795ecbd4c29439d
Frankie <zaihuazhao@163.com>
2019-09-25 21:35:20 +0800

    add clear_history api

--- beep/search/viewsets.py
+++ beep/search/viewsets.py
@@ -14,13 +14,21 @@ from .filters import SearchKeyWordFilter
 class SearchHistoryViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     """搜索历史
     list -- 我的搜索历史
+    clear_history -- 清空搜索记录
     """
     permission_classes = [IsAuthenticated]
     serializer_class = SearchHistorySerializer
 
     def get_queryset(self):
-        if self.action == 'list':
+        if self.action in ['list', 'clear_history']:
             return mm_SearchHistory.filter(user=self.request.user)
+
+    @action(detail=False, methods=['get'])
+    def clear_history(self, request):
+        queryset = self.get_queryset()
+        queryset.update(user=None)
+        return Response()
+
 
 
 class SearchKeyWordViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
