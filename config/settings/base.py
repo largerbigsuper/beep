9ed918439e620914497603d9024a26cb13c769bc
Frankie <zaihuazhao@163.com>
2019-10-25 23:56:07 +0800

    修改 cookie设置

--- config/settings/base.py
+++ config/settings/base.py
@@ -172,9 +172,9 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 # SECURITY
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-httponly
-SESSION_COOKIE_HTTPONLY = True
+# SESSION_COOKIE_HTTPONLY = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-httponly
-CSRF_COOKIE_HTTPONLY = True
+# CSRF_COOKIE_HTTPONLY = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-browser-xss-filter
 SECURE_BROWSER_XSS_FILTER = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#x-frame-options
