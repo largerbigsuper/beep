239c06e722767d0518a4b53daed311e254c68fe0
Frankie <zaihuazhao@163.com>
2019-10-25 21:40:10 +0800

    suport http

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -87,9 +87,9 @@ INSTALLED_APPS += ["django_extensions"]  # noqa F405
 # ------------------------------------------------------------------------------
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 CSRF_TRUSTED_ORIGINS = ['servicewechat.com', 'lhxq.top', 'beep.lhxq.top', '127.0.0.1', '127.0.0.1:8080']
 
 # 小程序
