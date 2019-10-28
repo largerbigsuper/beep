47e276ac054d7d945b209e347d00a4a5ee9455cd
Frankie <zaihuazhao@163.com>
2019-10-28 23:46:27 +0800

    增加聊天admin

--- beep/wechat_callback/admin.py
+++ beep/wechat_callback/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import WxBot, WxGroup, WxUser, WxMessage
+
+
+@admin.register(WxBot)
+class WxBotAdmin(admin.ModelAdmin):
+    
+    list_display = ['wxid', 'wx_alias', 'nickname', 'nonce']
+
+@admin.register(WxUser)
+class WxUserAdmin(admin.ModelAdmin):
+    
+    list_display = ['wxid', 'wx_alias', 'nickname']
+
+@admin.register(WxGroup)
+class WxGroupAdmin(admin.ModelAdmin):
+    
+    list_display = ['room_wxid', 'name', 'owner_wxid', 'member_count']
+
+@admin.register(WxMessage)
+class WxMessageAdmin(admin.ModelAdmin):
+    list_display = ['wxid_from', 'wxid_to', 'msg', ]
