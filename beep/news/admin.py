eb90878c5fb65b305b131091468c497a9d4ec8dc
Frankie <zaihuazhao@163.com>
2019-11-15 21:41:26 +0800

    增加 快讯关联后台功能

--- beep/news/admin.py
+++ beep/news/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 
-from .models import News, CrawledDocument
+from .models import News, CrawledDocument, mm_News
 
+
+def make_news_published(modeladmin, request, queryset):
+    """文章发布
+    """
+    queryset.update(status=mm_News.STATUS_PUBLISHED)
+make_news_published.short_description = '发布'
+
+def make_news_recall(modeladmin, request, queryset):
+    """文章撤回
+    """
+    queryset.update(status=mm_News.STATUS_RECALL)
+make_news_recall.short_description = '撤回'
+@admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(News, NewsAdmin)
+    list_display = ['id', 'title', 'published_at', 'status']
+    actions = [make_news_published, make_news_recall]
 
 @admin.register(CrawledDocument)
 class CrawledDocumentAdmin(admin.ModelAdmin):
