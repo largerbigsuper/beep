fd77b425a3b05f26f0a7237df8cefed51e428fca
Frankie <zaihuazhao@163.com>
2019-11-17 23:09:55 +0800

    blog admin content limited length to 100

--- beep/blog/admin.py
+++ beep/blog/admin.py
@@ -5,21 +5,22 @@ from .models import Topic, Blog, mm_Blog
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     
-    list_display = ['id', 'title', 'user', 'topic', 'content', 'is_anonymous', 'create_at', 'order_num']
+    list_display = ['id', 'title', 'user', 'topic', 'get_content', 'is_anonymous', 'create_at', 'order_num']
     list_filter = ['topic__topic_type', 'topic', 'is_top']
     list_editable = ['order_num']
     search_fields = ['title']
     autocomplete_fields = ['topic']
-    
 
     def get_queryset(self, request):
-        return mm_Blog.all()
+        return mm_Blog.select_related('topic', 'user').all()
 
     def delete_queryset(self, request, queryset):
         queryset.update(is_delete=True)
 
-    def content(self, obj):
+    def get_content(self, obj):
         return obj.content[:100]
+
+    get_content.short_description = '内容'
     
 
 
