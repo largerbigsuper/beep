f1fc91f8b4c38fd2eef6c80e4334f518b6d6f4cc
Frankie <zaihuazhao@163.com>
2019-11-21 01:28:55 +0800

    fix

--- beep/users/admin.py
+++ beep/users/admin.py
@@ -4,9 +4,9 @@ from django.contrib.auth import models, password_validation
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm
 from django.utils.translation import gettext, gettext_lazy as _
+from django.utils.html import mark_safe
 
-
-from .models import User
+from .models import User, mm_User, LableApply, mm_LableApply
 
 class MyUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
@@ -72,3 +72,38 @@ class MyUserAdmin(UserAdmin):
     )
 
 admin.site.register(User, MyUserAdmin)
+
+
+
+def make_apply_passed(modeladmin, request, queryset):
+    """设置通过申请
+    """
+    queryset.update(status=mm_LableApply.STATUS_PASSED)
+    for record in queryset:
+        record.user.label_type = record.lebel_type
+        record.user.save()
+
+make_apply_passed.short_description = '审核通过'
+
+def make_apply_failed(modeladmin, request, queryset):
+    """设置通过申请失败
+    """
+    queryset.update(status=mm_LableApply.STATUS_PASSED)
+
+make_apply_failed.short_description = '审核失败'
+
+
+@admin.register(LableApply)
+class LableApplyAdmin(admin.ModelAdmin):
+
+    list_display = ['user', 'lebel_type', 'image_tag', 'desc', 'status', 'create_at']
+    list_filter = ['lebel_type', 'status']
+    # list_editable = ['status']
+    search_fields = ['user__account']
+    actions = [make_apply_passed, make_apply_failed]
+    readonly_fields = ['image_tag']
+
+    def image_tag(self, obj):
+        return mark_safe('<img src="{}" width="150" height="150" />'.format(obj.image))
+
+    image_tag.short_description = '图片'
