5e53bcc978a44c47f841024c95f85d0e48d219f2
Frankie <zaihuazhao@163.com>
2019-11-24 18:52:07 +0800

    fix choices error

--- beep/activity/models.py
+++ beep/activity/models.py
@@ -277,7 +277,7 @@ class RewardPlan(models.Model):
         if self.total_luckyuser >= len(applys):
             selected_apply = applys
         else:
-            selected_apply = choices(self.total_luckyuser, applys)
+            selected_apply = choices(applys, k=self.total_luckyuser)
         result = []
         selected_pks = []
         for apply in selected_apply:
